cfb78c49e847731466faa9c4837c393b926d2cf1
ShampooBandit <79825068+ShampooBandit@users.noreply.github.com>
2021-05-24 12:51:01 -0700

    Update init_functions.js

--- init_functions.js
+++ init_functions.js
@@ -4,11 +4,12 @@ function initSquares()
    	var i;
    	for(i = 0; i < 1; i++)
    	{
-   		var elm = document.createElement("img");
+   		var elm = document.createElement("input");
    		var body = document.getElementByID("body");
 
    		//itemSpots[i] = new itemSpot();
 
+   		elm.type = "image";
    		elm.src = "Images/receptacle.png";
    		elm.addEventListener('mouseenter', scale(1,1));
    		elm.addEventListener('mouseleave', scale(0.5,0.5));
