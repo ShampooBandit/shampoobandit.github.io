8c3cad296a3b372429b00a46def229abdae2531d
ShampooBandit <79825068+ShampooBandit@users.noreply.github.com>
2021-05-24 12:40:40 -0700

    Update

--- init_functions.js
+++ init_functions.js
@@ -5,11 +5,14 @@ function initSquares()
    	for(i = 0; i < 1; i++)
    	{
    		var elm = document.createElement("img");
+   		var body = document.getElementByID("body");
 
    		//itemSpots[i] = new itemSpot();
 
    		elm.src = "Images/receptacle.png";
    		elm.addEventListener('mouseenter', scale(1,1));
    		elm.addEventListener('mouseleave', scale(0.5,0.5));
+
+   		body.appendChild(elm);
    	}
 }
