0be9879a4c27e563caca958b9331f216a53f2169
ShampooBandit <79825068+ShampooBandit@users.noreply.github.com>
2021-09-18 14:04:12 -0700

    sus

--- init_functions.js
+++ init_functions.js
@@ -13,7 +13,21 @@ function populateStorage() {
 }
 
 function firstTime() {
-	document.getElementById('text1').style.animation = 'driftUp 5s forwards';
+	var t = document.getElementById('text1'), t2 = document.getElementById('text2');
+	t.style.animation = 'driftUp 5s forwards';
+	t.addEventListener('animationend', () => {
+		t.style.animation = 'vanish 2s forwards';
+		t.addEventListener('animationend', () => {
+			t.classList.add('hidden');
+			t2.style.animation = 'driftUp 5s forwards';
+		})
+	})
+	t2.addEventListener('animationend', () => {
+		t2.style.animation = 'vanish 2s forwards';
+		t2.addEventListener('animationend', () => {
+			t2.classList.add('hidden');
+		})
+	})
 }
 
 function loadData() {
